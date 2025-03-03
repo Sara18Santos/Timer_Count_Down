a16b9e082fca696a07f471bff4fc4431f65a01dc
Sara18Santos <sataide.santos@gmail.com>
2025-03-03 19:32:38 +0000

    some corrections

--- Timer_Count_Down.py
+++ Timer_Count_Down.py
@@ -25,7 +25,7 @@ minutoEntry.place(x=1300, y=20)
 segudoEntry = Entry(root, width=3, font=("Arial", 18, ""), textvariable=segundo)
 segudoEntry.place(x=180, y=20)
 
-def sumbit():
+def submit():
     try: 
         temp = int(hora.get())*3600 + int(minuto.get())*60 + int(segundo.get())
     except:
@@ -54,6 +54,6 @@ def sumbit():
 
 # Button 
 btn_start = Button(root, text="Set Time Countdown", bd='5', command=submit)
-btn.place(x=70, y=120)
+btn_start.place(x=70, y=120)
 
 root.mainloop()
