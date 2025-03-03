69ef2db22297bca2686a6a725b8a4183093f88e9
Sara18Santos <sataide.santos@gmail.com>
2025-03-03 19:51:54 +0000

    more changes, some typos and changes in size of the window

--- Timer_Count_Down.py
+++ Timer_Count_Down.py
@@ -4,7 +4,7 @@ from tkinter import *
 from tkinter import messagebox
 
 root = Tk()
-root.geometry("300x250")
+root.geometry("300x230")
 root.title("Timer Count Down")
 
 # variables 
@@ -23,13 +23,14 @@ horaEntry.place(x=80, y=20)
 minutoEntry = Entry(root, width=4, font=("Arial", 18, ""), textvariable=minuto)
 minutoEntry.place(x=130, y=20)
 segundoEntry = Entry(root, width=4, font=("Arial", 18, ""), textvariable=segundo)
-segudoEntry.place(x=180, y=20)
+segundoEntry.place(x=180, y=20)
 
 def submit():
     try: 
         temp = int(hora.get())*3600 + int(minuto.get())*60 + int(segundo.get())
     except:
         print("Please input the right value.")
+        return 
     while temp > -1:
         mins, secs =  divmod(temp,60)  # divmod(firstvalue = temp//60, secondvalue = temp%60)
         # Converting the input entered in mins or secs to hours,
